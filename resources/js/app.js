a38e9b5f9aa8bc20fe27366f029176179bd88014
esteban-florez <eflorez077@gmail.com>
2024-11-04 06:41:28 -0400

    JavaScript necesario.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,25 @@ document.querySelectorAll('a[data-logout]').forEach(a => {
     document.querySelector('#logout').submit()
   })
 })
+
+if (window.location.pathname === '/appointments/pending') {
+  const buttons = document.querySelectorAll('[data-action]')
+  const form = document.querySelector('#assign-doctor-modal form')
+  const select = document.querySelector('select#doctor_id')
+  const modal = document.querySelector('.modal')
+  const submit = document.querySelector('.modal button[type="submit"]')
+
+  const checkButton = () => {
+    submit.disabled = select.value === ''
+  }
+
+  modal.addEventListener('hidden.bs.modal', () => select.value = '')
+
+  select.addEventListener('change', checkButton)
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => form.action = button.dataset.action)
+  })
+
+  checkButton()
+}
